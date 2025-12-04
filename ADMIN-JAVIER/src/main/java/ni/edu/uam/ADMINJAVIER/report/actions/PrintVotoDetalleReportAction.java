b4d9fbce94c2422056c532ae73710cd755358cde
sofia <sofiavargasayerdis>
2025-12-04 17:27:14 -0600

    Todos los reportes funcionan correctamente

--- ADMIN-JAVIER/src/main/java/ni/edu/uam/ADMINJAVIER/report/actions/PrintVotoDetalleReportAction.java
+++ ADMIN-JAVIER/src/main/java/ni/edu/uam/ADMINJAVIER/report/actions/PrintVotoDetalleReportAction.java
@@ -2,15 +2,21 @@ package ni.edu.uam.ADMINJAVIER.report.actions;
 
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.data.JRTableModelDataSource;
 import org.openxava.actions.JasperReportBaseAction;
+import org.openxava.tab.Tab;
 
+import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PrintVotoDetalleReportAction extends JasperReportBaseAction {
+    @Inject
+    private Tab tab;
+
     @Override
     protected JRDataSource getDataSource() throws Exception {
-        return new JREmptyDataSource();
+        return new JRTableModelDataSource(tab.getTableModel());
     }
 
     @Override
