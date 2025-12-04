fb41f9d9c23c383cc7a83746178940ec18e04d32
sofia <sofiavargasayerdis>
2025-12-04 16:50:40 -0600

    Reporte Facultades creado

--- ADMIN-JAVIER/src/main/java/ni/edu/uam/ADMINJAVIER/report/actions/PrintFacultadReportAction.java
+++ ADMIN-JAVIER/src/main/java/ni/edu/uam/ADMINJAVIER/report/actions/PrintFacultadReportAction.java
@@ -2,6 +2,7 @@ package ni.edu.uam.ADMINJAVIER.report.actions;
 
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.data.JRTableModelDataSource;
 import org.openxava.actions.JasperReportBaseAction;
 import org.openxava.tab.Tab;
 
@@ -17,65 +18,9 @@ public class PrintFacultadReportAction extends JasperReportBaseAction {
     @Inject
     private Tab tab;
 
-    // Bean que Jasper va a usar
-    public static class FacultadRow {
-        private String facultad;
-        private String descripcion;
-        private String estado;
-
-        public String getFacultad() { return facultad; }
-        public void setFacultad(String facultad) { this.facultad = facultad; }
-
-        public String getDescripcion() { return descripcion; }
-        public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-
-        public String getEstado() { return estado; }
-        public void setEstado(String estado) { this.estado = estado; }
-    }
-
     @Override
     protected JRDataSource getDataSource() throws Exception {
-        TableModel model = tab.getTableModel();
-
-        // Debug opcional
-        System.out.println("=== Columnas del módulo Facultad ===");
-        for (int i = 0; i < model.getColumnCount(); i++) {
-            System.out.println("Columna " + i + ": " + model.getColumnName(i));
-        }
-
-        int idxName  = -1;
-        int idxDesc  = -1;
-        int idxState = -1;
-
-        for (int i = 0; i < model.getColumnCount(); i++) {
-            String col = model.getColumnName(i);
-            if ("Name".equalsIgnoreCase(col)) {
-                idxName = i;
-            } else if ("Descripcion".equalsIgnoreCase(col) || "Description".equalsIgnoreCase(col)) {
-                idxDesc = i;
-            } else if ("State".equalsIgnoreCase(col)) {
-                idxState = i;
-            }
-        }
-
-        List<FacultadRow> data = new ArrayList<>();
-
-        for (int r = 0; r < model.getRowCount(); r++) {
-            FacultadRow row = new FacultadRow();
-
-            // Si no existe alguna columna, deja el valor vacío en lugar de reventar
-            String nombre      = idxName  != -1  ? String.valueOf(model.getValueAt(r, idxName))  : "";
-            String descripcion = idxDesc  != -1  ? String.valueOf(model.getValueAt(r, idxDesc))  : "";
-            String estado      = idxState != -1  ? String.valueOf(model.getValueAt(r, idxState)) : "";
-
-            row.setFacultad(nombre);
-            row.setDescripcion(descripcion);
-            row.setEstado(estado);
-
-            data.add(row);
-        }
-
-        return new JRBeanCollectionDataSource(data);
+        return new JRTableModelDataSource(tab.getTableModel());
     }
 
     @Override
@@ -84,7 +29,9 @@ public class PrintFacultadReportAction extends JasperReportBaseAction {
     }
 
     @Override
-    protected Map<String, Object> getParameters() throws Exception {
+    protected Map getParameters() throws Exception {
         return new HashMap<>();
     }
 }
+
+
