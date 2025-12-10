4c4a8953530b200943ecf6b8b551aee9f7fd1a77
sea2horses <thefuzegamer06@gmail.com>
2025-12-10 15:04:52 -0600

    Proyecto Wails

---
+++ DESKTOP-JAVIER/frontend/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import '../app.postcss';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<meta name="theme-color" content="#0b1220" />
+	<title>Jurado Reinado UAM</title>
+</svelte:head>
+
+<div class="min-h-screen">
+	{@render children()}
+</div>
